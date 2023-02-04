1175d40d0d86526bcc0d105e413f5944ec35389e
Salinacode <salinasubudhi103@gmail.com>
2023-02-04 14:18:08 +0530

    add student validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,6 @@
 class Student < ApplicationRecord
+	validates :studname, presence: true,uniqueness: { case_sensitive: false }, length: {minimum: 3,maximum: 25}
+	
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :studemail, presence: true,uniqueness: { case_sensitive: false }, length: {maximum: 105},format: { with: VALID_EMAIL_REGEX }
 end
