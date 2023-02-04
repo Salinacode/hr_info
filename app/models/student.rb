45111071606e6970bca6381140a1f10ccfcc3f82
Salinacode <salinasubudhi103@gmail.com>
2023-02-04 21:01:23 +0530

    add secure password to student email

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,4 +5,6 @@ class Student < ApplicationRecord
 	
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :studemail, presence: true,uniqueness: { case_sensitive: false }, length: {maximum: 105},format: { with: VALID_EMAIL_REGEX }
+
+	has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_04_093719) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_04_145352) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_04_093719) do
     t.string "studemail"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   create_table "users", force: :cascade do |t|
