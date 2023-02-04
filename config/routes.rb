cb578f8646ca6eaab1ff0546a87629916c372859
Salinacode <salinasubudhi103@gmail.com>
2023-02-04 22:23:05 +0530

    new student signup form

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root 'home#index'      #home-controller,index-action    
 # get '/home' => 'home#index'
   get 'about', to: 'page#about'
+  get 'signup', to: 'students#new'
+  resources :students, except: [:new]
 end
