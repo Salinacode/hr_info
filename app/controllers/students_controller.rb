5dd6ecf6e5324df2a3e67bbb0068769a9280c493
Salinacode <salinasubudhi103@gmail.com>
2023-02-05 16:08:21 +0530

    add students create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,19 @@ class StudentsController<ApplicationController
 	def new
 		@student = Student.new
 	end
+
+	def create
+	    @student = Student.new(student_params)
+	    if @student.save
+	      flash[:notice] = "Welcome to the Alpha Blog #{@student.studname}, you have successfully signed up"
+	      redirect_to articles_path
+	    else
+	      render 'new'
+	    end
+  	end
+
+	private
+	  def student_params
+	    params.require(:student).permit(:studname, :studemail, :password)
+	end
 end
