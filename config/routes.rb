624343c7941720f939f2d1404b20561848c05102
Salinacode <salinasubudhi103@gmail.com>
2023-02-07 17:56:18 +0530

    add login form for students

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'students#new'
   resources :students, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
