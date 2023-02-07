e26bc861636a5c8072c12c5915261f74627abb81
Salinacode <salinasubudhi103@gmail.com>
2023-02-07 20:27:07 +0530

    add log in and log out page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    student = Student.find_by(studemail: params[:session][:studemail].downcase)
+    
+    if student && student.authenticate(params[:session][:password])
+      session[:student_id] = student.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to student
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new'
+    end
   end
 
   def destroy
+    session[:student_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 
 end
