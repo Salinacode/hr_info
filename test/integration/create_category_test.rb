f2b9d359ea99ca07884996708c5af75946f77067
Salinacode <salinasubudhi103@gmail.com>
2023-02-16 20:35:40 +0530

    admin can only perform any changes in categies new file (admin student requirement and test)

--- test/integration/create_category_test.rb
+++ test/integration/create_category_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class CreateCategoryTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @admin_student = Student.create(studname: "sanjana", studemail: "sanju@example.com",
+                              password: "password", admin: true)
+    sign_in_as(@admin_student)
+  end
+
   test "get new category form and create category" do
     get "/categories/new"
     assert_response :success
